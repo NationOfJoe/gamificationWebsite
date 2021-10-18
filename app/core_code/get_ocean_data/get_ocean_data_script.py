281c698cd2529aeacef282307ea532793f59d146
Yoav Ekshtein <yoaveksh@gmail.com>
2021-10-18 17:06:49 +0100

    claenup and scoreboard

--- app/core_code/get_ocean_data/get_ocean_data_script.py
+++ app/core_code/get_ocean_data/get_ocean_data_script.py
@@ -7,10 +7,25 @@ def get_Ocean_object(data):
     OceanId = data['oceanid']
     spot_handler_instance = spot_api()
     URL="https://api.spotinst.io/ocean/aws/k8s/cluster/{oceanClusterId}".format(oceanClusterId=OceanId)
-    resp = json.loads(spot_handler_instance.rest_session.get(
-        url=URL
-    ).text)['response']['items'][0]
-    return str(resp)
+    try:
+        raw_resp = json.loads(spot_handler_instance.rest_session.get(
+            url=URL
+        ).text)['response']['items'][0]
+        resp = str(raw_resp)
+        if resp.__contains__('autoHeadroomPercentage'):
+            print ('it does')
+            # headroom = raw_resp['autoHeadroomPercentage']
+            headroom = 'aa'
+            print('resp %s' % resp)
+            print('headroom %s' % headroom)
+        else:
+            headroom = None
+            print ('no headroom')
+    except Exception as e:
+        print ('Exception is %s' % e)
+        resp = None
+        headroom = None
+    return resp, headroom
 
 def get_Ocean_heartbeat(data):
     orgid = "6060798" + str(data['orgid'])
--- app/routes.py
+++ app/routes.py
@@ -2,13 +2,11 @@ from . import app
 from flask import request, render_template, render_template_string, send_file
 from flask_menu import Menu, register_menu
 import json
-from .core_code.calculate import calculate_class
 from .core_code.get_monthly_savings.monthly import get_monthly_report , months_map
-from .core_code.get_monthly_savings.monthly_trend import monthly_trend
 from .core_code.get_ocean_data.get_ocean_data_script import *
 from .core_code.get_ocean_data.tinydb_handler import *
 from .core_code.interrupts.interrups import org_interrupts_report
-from .analyserLog.GetFullSpotAnalyserOrg import getAllPotentialSavingsData
+
 from.constants import init_globals
 
 import datetime
@@ -33,69 +31,7 @@ def index():
 def ocean_feat():
     return render_template('ocean_input.html')
 
-@app.route('/monthly_savings')
-@register_menu(app, '.monthly_savings', 'Monthly Savings report', order=3)
-def month_report():
-    return render_template('monthly_savings_input.html')
 
-@app.route('/monthly_savings_trend')
-@register_menu(app, '.monthly_savings_trend', 'Monthly Savings trend report', order=4)
-def month_trend_report():
-    return render_template('monthly_trend_input.html')
-
-@app.route('/interrupts')
-@register_menu(app, '.interrupt_report', 'Interruptions', order=5)
-def interrupt_report():
-    return render_template('interrupt_input.html')
-
-@app.route('/analyser_input')
-@register_menu(app, '.analyser_input', 'Spot Analyser', order=6)
-def analyser_input():
-    return render_template('analyser_input.html')
-
-
-@app.route('/get_monthly_savings', methods=['POST'])
-def get_monthly_savings():
-    global filename
-    headers = dict(request.headers)
-    data = request.form
-    right_now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    filename = '/var/tmp/monthly_results_{}.csv'.format(right_now)
-    account_answer = get_monthly_report(filename, data['orgid'], data['from-months'], data['to-months'])
-
-    print(data)
-
-    print (account_answer)
-
-    return render_template(
-        'savings_answer.html',
-        savings = account_answer
-    )
-
-@app.route('/interrupt_report', methods=['POST'])
-def get_monthly_interrupts():
-    global filename
-    headers = dict(request.headers)
-    data = request.form
-    right_now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    filename = '/var/tmp/interrupts_results_{}.csv'.format(right_now)
-    account_answer, org_name = org_interrupts_report(filename, data['orgid'], data['months'])
-
-    print(data)
-
-    chosen_month = '{0} {1}'.format(
-        months_map.get(data['months'].split('-')[1]),
-        data['months'].split('-')[0]
-    )
-
-    print (account_answer)
-
-    return render_template(
-        'interrupts_answer.html',
-        savings = account_answer,
-        month_chosen = chosen_month,
-        org_name = org_name
-    )
 
 @app.route('/get_ocean_data', methods=['POST'])
 def get_ocean_data():
@@ -105,7 +41,7 @@ def get_ocean_data():
     headers = dict(request.headers)
     data = request.form
     print(data)
-    account_answer = get_Ocean_object(data)
+    account_answer, headroom = get_Ocean_object(data)
     if action == 'Search':
         print(data['oceanid'])
         raw_text = '''
@@ -115,12 +51,18 @@ def get_ocean_data():
          }
         ''' % (data['oceanid'], account_answer)
         print(raw_text)
-        db_data_to_save = json.loads(raw_text)
         db_instance.save_data(
             ocean_id=data['oceanid'],
             key_name='ocean_data',
-            key_value=account_answer
+            key_value=account_answer or "can'\t say"
         )
+        if headroom:
+            db_instance.save_data(
+                ocean_id=data['oceanid'],
+                key_name='headroom',
+                key_value=headroom or "can'\t say"
+            )
+
 
     elif action == 'check_status':
         heartbeat = get_Ocean_heartbeat(data)
@@ -147,13 +89,9 @@ def get_ocean_data():
     db_instance.save_to_s3()
     return render_ocean_template(data['oceanid'])
 
-def render_ocean_template(ocean_id=None):
-
-    db_instance = tinydb_handler_class()
-    ocean_json_data = db_instance.get_data_by_key(ocean_id, 'ocean_data') or 'None'
-    heartbeat = db_instance.get_data_by_key(ocean_id, 'heartbeat') or 'Can\'t say'
-    num_vng = db_instance.get_data_by_key(ocean_id, 'VNGs') or 'None'
-
+def get_scores_in_order(db_instance=None):
+    if not db_instance:
+        db_instance = tinydb_handler_class()
     scores = []
     score_headlines = ['team_name', 'score', 'oceanid']
     scores_raw = db_instance.get_all_data()
@@ -167,115 +105,35 @@ def render_ocean_template(ocean_id=None):
                 }
             )
         scores.append(temp_score_record)
+    return scores
 
+def render_ocean_template(ocean_id=None):
 
+    db_instance = tinydb_handler_class()
+    ocean_json_data = db_instance.get_data_by_key(ocean_id, 'ocean_data') or 'None'
+    heartbeat = db_instance.get_data_by_key(ocean_id, 'heartbeat') or 'Can\'t say'
+    num_vng = db_instance.get_data_by_key(ocean_id, 'VNGs') or 'None'
+    headroom = db_instance.get_data_by_key(ocean_id, 'headroom') or 'None'
+
+    scores = get_scores_in_order(db_instance)
 
     return render_template(
         'ocean_input.html',
         ocean_data = ocean_json_data,
         cluster_status=heartbeat,
         vng=num_vng,
-        scores=scores
+        scores=scores,
+        headroom=headroom
 
     )
 
-@app.route('/monthly_trend', methods=['POST'])
-def get_monthly_savings_trend():
-    global filename
-    global progress
-    progress = 0
-    headers = dict(request.headers)
-    data = request.form
-    print (data)
-    right_now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    filename = '/var/tmp/monthly_trend_results_{}.csv'.format(right_now)
-    account_answer = monthly_trend(filename, data['orgid'])
 
-    print(data)
 
-    print (account_answer)
-
-    return render_template(
-        'savings_trend_answer.html',
-        savings = account_answer
-    )
-
-@app.route('/analyser_calc', methods=['POST'])
-def get_analyser_report():
-    global filename
-    headers = dict(request.headers)
-    global progress
-    progress = 0
-    data = request.form
-    right_now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    filename = '/var/tmp/analyser_report_{}.csv'.format(right_now)
-    account_answer, fields, org_name, filename = getAllPotentialSavingsData(filename, data['orgid'])
-
-    print(data)
-
-    print (account_answer)
-
-    return render_template(
-        'analyser_answer.html',
-        savings = account_answer,
-        fields = fields,
-        org_name = org_name,
-        filename = filename.split('/')[-1]
-    )
-
-@app.route('/download_csv', methods=['get'])
-def download_csv():
-
-    global filename
-    print (filename)
-    results_file_name = 'results.csv'
-    results_file_name_raw = request.args.get('file_name')
-    if results_file_name_raw:
-        results_file_name = results_file_name_raw
-    return send_file(
-        filename,
-        attachment_filename=results_file_name,
-        cache_timeout=1,
-        as_attachment=True
-    )
-
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    headers = dict(request.headers)
-    data = request.form
-
-    headers_tbl = ['Basic']
-
-    if 'standard' in data.keys():
-        headers_tbl.append('standard')
-    if 'aggressive' in data.keys():
-        headers_tbl.append('aggressive')
-    if 'veryaggressive' in data.keys():
-        headers_tbl.append('veryaggressive')
-
-    calculator = calculate_class(data)
-    calculator.do_calc(headers_tbl)
-    json_report = json.dumps(calculator.report)
-    print (','.join(data.keys()))
-
-    return render_template(
-        'report.html',
-        customer_name=calculator.customer_name,
-        suggestions = calculator.aabb,
-        aa=int(round(calculator.ri_ec2, 0)),
-        # bb=int(round(calculator.total_potential, 0)),
-        bb = calculator.tier,
-        total_savings = int(round(12 * calculator.total_potential, 0)),
-        spot_save = int(round(calculator.total_cost, 0)),
-        json_data_report = json_report,
-        headers_tbl=headers_tbl
-
-    )
-
-@app.route('/tiers')
-@register_menu(app, '.tiers', 'Saving Tiers', order=7)
-def sf_input():
-    return render_template('inputs.html')
+@app.route('/scoreboard')
+@register_menu(app, '.scoreboard', 'Scoreboard', order=8)
+def scoreboard():
+    scores = get_scores_in_order()
+    return render_template('scoreboard.html', scores=scores)
 
 @app.route('/store', methods=['GET'])
 def store():
@@ -293,12 +151,7 @@ def update_team_score():
     db_instance = tinydb_handler_class()
     headers = dict(request.headers)
     args = request.args
-    # print (args)
-    # args = request.form
     print (args)
-    result = []
-    # team_name = args.get('team_name') or None
-    # ocean_id = args.get('ocean_id') or None
     team_name = args.get('team_name') or None
     ocean_id = args.get('ocean_id') or None
     print (ocean_id)
